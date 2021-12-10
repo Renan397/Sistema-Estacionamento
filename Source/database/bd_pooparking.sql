0a32fbcd7c94cd67de54ebe5fc5c3c17bec866c4
ThiPM <thiago.martins@aluno.ifsp.edu.br>
2021-12-10 14:28:36 -0300

    z

--- Source/database/bd_pooparking.sql
+++ Source/database/bd_pooparking.sql
@@ -11,16 +11,3 @@ data_entrada_veiculo_cli date,
 hora_entrada_veiculo_cli time,
 hora_saida_veiculo_cli time
 );
-
-#Modelo de insert
-insert into tb_agenda_veiculos (nome_cli, telefone_cli, veiculo_cli, placa_veiculo_cli, data_entrada_veiculo_cli, hora_entrada_veiculo_cli) 
-values ("Renan", "11962470654", "Ford KA Branco Lamaçento", "ABC1234", 20210821, 123500);
-
-#Modelo de update
-update tb_agenda_veiculos set hora_saida_veiculo_cli = 184000 where placa_veiculo_cli = "ABC1234";
-
-#Modelo de delete
-delete from tb_agenda_veiculos where id_cli = 1;
-
-#Modelo de select
-select *from tb_agenda_veiculos;
