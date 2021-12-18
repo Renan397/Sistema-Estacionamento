f113bf7c880fbf528b0db9ef7d50b2a577e7a94e
Renan397 <renan.santos@aluno.ifsp.edu.br>
2021-12-17 21:43:00 -0300

    Finalizado

--- Source/database/atualizarAgenda.class.php
+++ Source/database/atualizarAgenda.class.php
@@ -11,6 +11,34 @@ if ($_REQUEST) {
     $hora_saida = $_REQUEST["hora_saida_cli"];
     $taxa_hora = 6.00;
 
+    function registraPagamento($codigoCli, $hora_saida, $taxa_hora)
+    {
+        try {
+            //Registro e resultado do pagamento
+            $select =
+                "
+                SELECT * FROM tb_agenda_veiculos WHERE id_cli = '{$codigoCli}';
+    ";
+            $query = Connect::getInstance()->query($select);
+            $retorno = $query->fetchAll(PDO::FETCH_ASSOC);
+            $hora_entrada = $retorno[0]["hora_entrada_veiculo_cli"];
+            $H_entrada_number = intval(substr("{$hora_entrada}", 0, -6));
+            $H_saida_number = intval(substr("{$hora_saida}", 0, -3));
+            $horas_a_pagar = ($H_saida_number - $H_entrada_number) * $taxa_hora;
+            $insertPagamento =
+                "
+    UPDATE tb_agenda_pagamento SET hora_saida_veiculo_cli = '{$hora_saida}' WHERE tb_agenda_pagamento.id_cli = '{$codigoCli}';
+    UPDATE tb_agenda_pagamento SET pagamento_total = '{$horas_a_pagar}' WHERE tb_agenda_pagamento.id_cli = '{$codigoCli}';
+    ";
+            $query = Connect::getInstance()->query($insertPagamento);
+            $_SESSION["retornoPagamento"] = "
+    <center><p style='color:black; font-size: 18px;'>O total a ser pago é: <span style='color: #007000;'>R$ $horas_a_pagar,00</span> $H_saida_number e $H_entrada_number</p></center>
+    ";
+        } catch (PDOException $exception) {
+            die($exception);
+        }
+    };
+
     function deletaAgenda($placaCli)
     {
         try {
@@ -27,34 +55,6 @@ if ($_REQUEST) {
         }
     };
 
-    function registraPagamento($codigoCli, $hora_saida, $taxa_hora)
-    {
-        try {
-            //Registro e resultado do pagamento
-            $select =
-                "
-                SELECT * FROM tb_agenda_veiculos WHERE id_cli = '{$codigoCli}';
-    ";
-            $query = Connect::getInstance()->query($select);
-            $retorno = $query->fetchAll(PDO::FETCH_ASSOC);
-            $hora_entrada = $retorno[0]["hora_entrada_veiculo_cli"];
-            $H_entrada_number = intval(substr("{$hora_entrada}", 0, -6));
-            $H_saida_number = intval(substr("{$hora_saida}", 0, -3));
-            $horas_a_pagar = ($H_saida_number - $H_entrada_number) * ($taxa_hora);
-            $insertPagamento =
-                "
-    UPDATE tb_agenda_pagamento SET hora_saida_veiculo_cli = '{$hora_saida}' WHERE tb_agenda_pagamento.id_cli = '{$codigoCli}';
-    UPDATE tb_agenda_pagamento SET pagamento_total = '{$horas_a_pagar}' WHERE tb_agenda_pagamento.id_cli = '{$codigoCli}';
-    ";
-            $query = Connect::getInstance()->query($insertPagamento);
-            $_SESSION["retornoPagamento"] = "
-    <center><p style='color:black; font-size: 18px;'>O total a ser pago é: <span style='color: #007000;'>R$ $horas_a_pagar,00</span></p></center>
-    ";
-        } catch (PDOException $exception) {
-            die($exception);
-        }
-    };
-
+    registraPagamento($codigoCli, $hora_saida, $taxa_hora);
     deletaAgenda($placaCli);
-    registraPagamento($codigoCli, $placaCli, $hora_saida, $taxa_hora);
 };
